e748b2b6ebe8a4b4958df7e15a9b1460c57ed4a9
Edithhh <vohoanghuy8811@gmail.com>
2024-11-22 23:39:11 +0700

    'modifiedBE'

--- BE/history/printing/routes.py
+++ BE/history/printing/routes.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-
+from db import db
 
 router = APIRouter()
 
 @router.get("/history/printing/{student_id}")
-async def get_printing_history(student_id: str):
-    return {"student_id": student_id, "print_history": ["print1", "print2"]}
+async def get_printing_log(student_id: str):
+    results = await db.fetch_all("SELECT * FROM printing_ord WHERE student_id = :student_id", {"student_id": student_id})
+    if not results:
+        raise HTTPException(status_code=404, detail="No printing log found for the specified student.")
+
+    return {'success': True, 'data': results}
--- BE/printers/routes.py
+++ BE/printers/routes.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-
+from db import db
 
 router = APIRouter()
 
 @router.get("/printers/{campus}")
 async def get_printers(campus: str):
-    return {"campus": campus, "printers": ["printer1", "printer2"]}
+    query = """
+        SELECT printer.id, printer.room, printer.info, COUNT(printing_ord.id) AS queue
+        FROM printer
+        LEFT JOIN printing_ord ON printer.id = printing_ord.printer_id 
+        AND printing_ord.status = 'pending'
+        WHERE printer.campus = :campus and printer.status = 'available'
+        GROUP BY printer.id
+    """
+    results = await db.fetch_all(query, {"campus": campus})
+    
+    if not results:
+        raise HTTPException(status_code=404, detail="No printers found for the specified campus.")
+
+    # Returning the response with the 'queue' attribute
+    return {'success': True, 'data': results}
